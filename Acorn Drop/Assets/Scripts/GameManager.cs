2a37283f17d9e925038fcb19752ecde2d95fa0e9
yanath126 <yanath@iu.edu>
2025-10-16 18:23:25 -0400

    Adding mouse-vine interaction, getting the vines to be destroyed after mouse swipe

--- Acorn Drop/Assets/Scripts/GameManager.cs
+++ Acorn Drop/Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     bool isDragging = false;
     Vector3 ImousePosition;
+    HingeJoint2D joint;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,27 +16,51 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) // when player clicks mouse
+        if (Input.GetButtonDown("Fire1")) // when player clicks mouse
         {
             ImousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.position = new Vector3(ImousePosition.x, ImousePosition.y, 0);
+            //Debug.Log("Is Clicking");
             isDragging = true;
         }
 
-        if (Input.GetMouseButton(0) && isDragging) // when holding down the mouse, player begins to cut the vine
+        if (Input.GetButton("Fire1") && isDragging) // when holding down the mouse, player begins to cut the vine
         {
             Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.position = new Vector3(ImousePosition.x, ImousePosition.y, 0);
 
-            Debug.Log("Dragging");
+            //Debug.Log("Dragging");
             //cut
             ImousePosition = currentMousePosition;
         }
-        
-        if (Input.GetMouseButtonUp(0)) //after mouse click releases
+
+        if (Input.GetButtonUp("Fire1")) //after mouse click releases
         {
             isDragging = false;
         }
 
     }
+    
+    
+    void destroyVines(GameObject gameObject)
+    {
+        joint.gameObject.GetComponent<HingeJoint2D>();
+        if (joint != null)
+        {
+            Destroy(joint);
+        }
+
+        Destroy(gameObject);
+        //destroy the rest of the rope
+        Rigidbody2D rgb2 = gameObject.GetComponent<Rigidbody2D>();
+        if (rgb2 != null)
+        {
+            //destroy the surrounding vine links
+        }
+    }
+
+
+
+
+
 }
