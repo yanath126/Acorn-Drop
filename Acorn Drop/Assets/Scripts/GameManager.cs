6b1d8bed4c6a78002569ddccbeb447e2551a648e
yanath126 <yanath@iu.edu>
2025-10-24 15:23:37 -0400

    fixed all vine problems as well as the cutter issues

--- Acorn Drop/Assets/Scripts/GameManager.cs
+++ Acorn Drop/Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 using UnityEngine.SceneManagement;
 
+
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject cutterPrefab;
@@ -16,6 +17,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] int level = 0;
     public static GameManager instance;
     public bool LevelLost = false;
+    GameObject cutter;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
@@ -40,35 +42,48 @@ public class GameManager : MonoBehaviour
         if (Input.GetButtonDown("Fire1")) // when player clicks mouse
         {
             ImousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector3(ImousePosition.x, ImousePosition.y, 0);
+
+            //transform.position = new Vector3(ImousePosition.x, ImousePosition.y, 0);
             //Debug.Log("Is Clicking");
             isDragging = true;
+
+            spawnCutter();
+            Debug.Log(ImousePosition);
+            //Debug.Break();
         }
 
         if (Input.GetButton("Fire1") && isDragging) // when holding down the mouse, player begins to cut the vine
         {
-            Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector3(ImousePosition.x, ImousePosition.y, 0);
+            Vector3 endPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            stretchCutter(ImousePosition, endPosition);
+            //transform.position = new Vector3(ImousePosition.x, ImousePosition.y, 0);
 
             //Debug.Log("Dragging");
             //check for a swipe and destroy the vines
-            spawnCutter(ImousePosition, currentMousePosition);
-            ImousePosition = currentMousePosition;
+            
+            // ImousePosition = currentMousePosition;
         }
 
         if (Input.GetButtonUp("Fire1")) //after mouse click releases
         {
-
+            
             isDragging = false;
         }
         
     }
-    void spawnCutter(Vector3 start, Vector3 end)
+    void spawnCutter()
     {
-        GameObject cutter = Instantiate(cutterPrefab, new Vector3(ImousePosition.x, ImousePosition.y, 0), Quaternion.identity);
-        EdgeCollider2D edge = cutter.GetComponent<EdgeCollider2D>();
-        edge.points = new Vector2[] { start, end };
+        cutter = Instantiate(cutterPrefab, new Vector3(ImousePosition.x, ImousePosition.y, 0), Quaternion.identity);
+        
+        //Debug.Break();
+    }
 
+    void stretchCutter(Vector3 start, Vector3 end)
+    {
+        EdgeCollider2D edge = cutter.GetComponent<EdgeCollider2D>();
+        float absoluteOffsetx = (end.x - ImousePosition.x);
+        float absoluteOffsety = (end.y - ImousePosition.y);
+        edge.points = new Vector2[] { new Vector2(0,0), new Vector2(absoluteOffsetx, absoluteOffsety)};
     }
 
     public void restartLevel()
@@ -81,7 +96,7 @@ public class GameManager : MonoBehaviour
     {
         level += number;
         Debug.Log(level);
-        SceneManager.LoadScene(level);
+        SceneManager.LoadScene(level, LoadSceneMode.Single);
     }
     public void AddButterfly()
     {
