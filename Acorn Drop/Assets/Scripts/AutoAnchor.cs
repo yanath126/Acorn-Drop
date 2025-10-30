2cb39022553332e1792b0e2e01a88e089f277b45
yanath126 <yanath@iu.edu>
2025-10-30 12:56:02 -0400

    adding vine hooking to he anchor and acorn

--- Acorn Drop/Assets/Scripts/AutoAnchor.cs
+++ Acorn Drop/Assets/Scripts/AutoAnchor.cs
@@ -5,6 +5,8 @@ public class AutoAnchor : MonoBehaviour
     [SerializeField] Transform vinePrefab;
     [SerializeField] int vineLength = 1;
     [SerializeField] Vector2 acornAnchorOffset = new Vector2(0, 0.5f);
+    [SerializeField] Rigidbody2D previousVine;
+    [SerializeField] float vineHeight = 0.5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,14 +22,38 @@ public class AutoAnchor : MonoBehaviour
     {
         if (collision.gameObject.tag == "Acorn")
         {
-            HingeJoint2D newHinge = collision.gameObject.AddComponent<HingeJoint2D>();
-            newHinge.autoConfigureConnectedAnchor = false;
-            newHinge.connectedBody = null;
-            newHinge.connectedAnchor = transform.position;
-            newHinge.anchor = acornAnchorOffset;
-            newHinge.enableCollision = false;
-            Transform vine = Instantiate(vinePrefab, transform.position, Quaternion.identity);
-            vine.SetParent(transform);
+            previousVine = null;
+            for (int i = 0; i < vineLength; i++)
+            {
+                Transform vine = Instantiate(vinePrefab, transform.position, Quaternion.identity);
+                Rigidbody2D vinerb = vine.GetComponent<Rigidbody2D>();
+
+                HingeJoint2D hinge = vine.gameObject.AddComponent<HingeJoint2D>();
+                hinge.autoConfigureConnectedAnchor = false;
+
+                if (i == 0)
+                {
+                    hinge.connectedBody = null;
+                    hinge.connectedAnchor = transform.position;
+                }
+                else
+                {
+                    hinge.connectedBody = previousVine;
+                    hinge.connectedAnchor = new Vector2(0, -vineHeight / 2f);
+                }
+                hinge.anchor = new Vector2(0, vineHeight / 2f);
+                hinge.enableCollision = false;
+
+                previousVine = vinerb;
+                vine.SetParent(transform);
+            }
+                HingeJoint2D newHinge = collision.gameObject.AddComponent<HingeJoint2D>();
+                newHinge.autoConfigureConnectedAnchor = false;
+                newHinge.connectedBody = previousVine;
+                newHinge.connectedAnchor = new Vector2(0, -vineHeight/2f);
+                newHinge.anchor = acornAnchorOffset;
+                newHinge.enableCollision = false;
+            
         }
     }
 }
