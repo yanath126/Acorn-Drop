d5ad822ed68879aa92ca3ebeab4e1e48f91b48e9
yanath126 <yanath@iu.edu>
2025-11-03 17:54:00 -0500

    changed scope: no longer need auto anchor mecahanic

--- Acorn Drop/Assets/Scripts/Vines.cs
+++ Acorn Drop/Assets/Scripts/Vines.cs
@@ -19,9 +19,8 @@ public class Vines : MonoBehaviour
         if (collision.gameObject.tag == "Cutter")
         {
             Destroy(GetComponent<HingeJoint2D>());
-            // Destroy(GetComponent<Rigidbody2D>());
             Destroy(gameObject);
-            Debug.Log("destroyed vine segment");
+            //Debug.Log("destroyed vine segment");
         }
     }
 }
