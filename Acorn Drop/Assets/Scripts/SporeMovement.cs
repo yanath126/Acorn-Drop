8df80dce79bbbff0f93c13ef1965b8dfaf845888
yanath126 <yanath@iu.edu>
2025-11-13 13:34:23 -0500

    adding moving psores

---
+++ Acorn Drop/Assets/Scripts/SporeMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class SporeMovement : MonoBehaviour
+{
+
+    [SerializeField] float speed = 2f;
+    [SerializeField] float distance = 3f;
+    [SerializeField] Transform RightSpore;
+    [SerializeField] Transform LeftSpore;
+
+    Vector3 startPosLeft;
+    Vector3 startPosRight;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosLeft = LeftSpore.position;
+        startPosRight = RightSpore.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movement = Mathf.PingPong(Time.deltaTime * speed, 1f);
+        Vector3 LeftOffset = new Vector3(movement*distance, movement*distance, 0);
+        Vector3 RightOffset = new Vector3(-movement*distance, -movement*distance, 0);
+    }
+
+}
